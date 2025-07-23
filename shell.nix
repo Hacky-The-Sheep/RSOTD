c2389f875477ad8b760984824eeaa2a5a9a5d505
Hacky <jon.nguyen7@protonmail.com>
2025-07-22 23:23:26 -0500

    fixed the nix shell

--- shell.nix
+++ shell.nix
@@ -1,15 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-with pkgs;
-
-mkShell rec {
-  nativeBuildInputs = [
-    pkg-config
+{ pkgs ? import <nixpkgs> {}
+}: pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    cargo
+    rustc
+    rust-analyzer
+    rustfmt
+    openssl
+    pkgconf
+    vscode-langservers-extracted
   ];
-  buildInputs = [
-    udev alsa-lib vulkan-loader
-    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
-    libxkbcommon wayland # To use the wayland feature
-  ];
-  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
