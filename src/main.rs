26717a5051595a978e856757484b6a6e9bbb0483
Hacky <jon.nguyen7@protonmail.com>
2025-07-22 23:44:11 -0500

    testing colors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod saints;
 
 use chrono::Local;
+use colored::*;
 
 fn main() {
     // Get the date and time
@@ -12,9 +13,11 @@ fn main() {
 
     if let Some(month) = months.get(&parsed_month) {
         if let Some(saint) = month.get(&parsed_date) {
-            println!("\nSaint of the Day:\n{}", saint);            
+            println!("\nSaint of the Day:\n{}", saint);
+            let outside = "Martyr".truecolor(243, 139, 168).bold().italic();
+            println!("{}", outside);
         } else {
-            println!("\nNo saint of the day. Go pray your rosary");
+            println!("\nNo saint of the day. Go pray your rosary 󰕹 ");
         }
     }
 }
